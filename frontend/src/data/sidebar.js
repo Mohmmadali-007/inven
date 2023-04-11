afe6552996e009edfaf1b2d4254064b8989f8038
Mohmmadali <aglodiyamohmmadali@gmail.com>
2023-04-11 13:17:12 +0530

    Initial commit

---
+++ frontend/src/data/sidebar.js
@@ -0,0 +1,41 @@
+import { FaTh, FaRegChartBar, FaCommentAlt, FaIcons } from "react-icons/fa";
+import { BiImageAdd } from "react-icons/bi";
+
+const menu = [
+  {
+    title: "Dashboard",
+    icon: <FaTh />,
+    path: "/dashboard",
+  },
+  {
+    title: "Add Product",
+    icon: <BiImageAdd />,
+    path: "/add-product",
+  },
+  {
+    title: "Account",
+    icon: <FaRegChartBar />,
+    childrens: [
+      {
+        title: "Profile",
+        path: "/profile",
+      },
+      {
+        title: "Edit Profile",
+        path: "/edit-profile",
+      },
+    ],
+  },
+  {
+    title: "Report Bug",
+    icon: <FaCommentAlt />,
+    path: "/contact-us",
+  },
+  {
+    title: "Invoice",
+    icon: <FaIcons/>,
+    path:"/invoice"
+  }
+];
+
+export default menu;
